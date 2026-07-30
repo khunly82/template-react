5a9f772f0fb26bfa3b973c3880a83afc4dd29968
Khun <khunly@bstorm.be>
2026-07-30 10:40:44 +0200

    feat: Update Game scene to initialize image and handle keyboard input

--- src/game/scenes/Game.js
+++ src/game/scenes/Game.js
@@ -3,18 +3,28 @@ import { Scene } from 'phaser';
 
 export class Game extends Scene
 {
+    /** @type { Phaser.GameObjects.Image } */
+    image = null
     constructor ()
     {
         super('Game');
     }
 
-    init() {
-        this.add.image(0, 0, 'flappy')
+    init() 
+    {
+        this.image = this.add.image(500, 500, 'flappy')
+        this.image.setScale(0.1, 0.1)
     }
 
     create ()
     {
         EventBus.emit('current-scene-ready', this);
+        this.input.keyboard.on('keydown', e => {
+            if(e.code === 'Space') {
+                if(this.image)
+                    this.image.setPosition(this.image.x, this.image.y - 1)
+            }
+        })
     }
 
 }
