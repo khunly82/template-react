1e84c630aa6ebf705a068e8953dcd7dfe23d7118
Khun <khunly@bstorm.be>
2026-07-30 10:15:46 +0200

    Refactor code structure for improved readability and maintainability

--- src/game/main.js
+++ src/game/main.js
@@ -1,7 +1,6 @@
 import { Boot } from './scenes/Boot';
 import { Game } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
-import { MainMenu } from './scenes/MainMenu';
 import Phaser from 'phaser';
 import { Preloader } from './scenes/Preloader';
 
@@ -16,7 +15,6 @@ const config = {
     scene: [
         Boot,
         Preloader,
-        MainMenu,
         Game,
         GameOver
     ]
